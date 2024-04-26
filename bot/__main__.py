20ad8ec5500c1df16df6ba54c4efb1a2621ca7e6
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2024-04-26 20:41:08 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,14 @@
-#    This file is part of the CompressorQueue distribution.
-#    Copyright (c) 2021 Danish_00
-#    Script Improved by Zylern
+# This file is part of the CompressorQueue distribution.
+# Copyright (c) 2021 Danish_00
+# Script Improved by Zylern
 
-
+import itertools
 from . import *
 from .config import *
 from .worker import *
 from .devtools import *
 from .FastTelethon import *
+
 LOGS.info("Starting...")
 
 try:
@@ -22,251 +23,7 @@ except Exception as er:
 async def _(e):
     await start(e)
 
-
-@bot.on(events.NewMessage(pattern="/setcode"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await coding(e)
-
-
-@bot.on(events.NewMessage(pattern="/getcode"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getcode(e)
-
-
-@bot.on(events.NewMessage(pattern="/showthumb"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getthumb(e)
-
-
-@bot.on(events.NewMessage(pattern="/logs"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getlogs(e)
-
-
-@bot.on(events.NewMessage(pattern="/cmds"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await zylern(e)
-
-
-@bot.on(events.NewMessage(pattern="/ping"))
-async def _(e):
-    await up(e)
-
-
-@bot.on(events.NewMessage(pattern="/sysinfo"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await sysinfo(e)
-
-
-@bot.on(events.NewMessage(pattern="/leech"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await dl_link(e)
-
-
-@bot.on(events.NewMessage(pattern="/help"))
-async def _(e):
-    await ihelp(e)
-
-
-@bot.on(events.NewMessage(pattern="/renew"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await renew(e)
-
-
-@bot.on(events.NewMessage(pattern="/clear"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await clearqueue(e)
-
-
-@bot.on(events.NewMessage(pattern="/speed"))
-async def _(e):
-    await test(e)
-    
-    
-
-########## Direct ###########
-
-@bot.on(events.NewMessage(pattern="/eval"))
-async def _(e):
-    await eval(e)
-
-@bot.on(events.NewMessage(pattern="/bash"))
-async def _(e):
-    await bash(e)
-
-
-######## Callbacks #########
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats(.*)")))
-async def _(e):
-    await stats(e)
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"skip(.*)")))
-async def _(e):
-    await skip(e)
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile("help")))
-async def _(e):
-    await help(e)
-
-########## AUTO ###########
-
-@bot.on(events.NewMessage(incoming=True))
-async def _(event):
-        if not event.photo:
-            return
-        os.system("rm thumb.jpg")
-        await event.client.download_media(event.media, file="/bot/thumb.jpg")
-        await event.reply("**Thumbnail Saved Successfully.**")
-
-
-@bot.on(events.NewMessage(incoming=True))
-async def _(e):
-from . import *
-from .config import *
-from .worker import *
-from .devtools import *
-from .FastTelethon import *
-LOGS.info("Starting...")
-
-try:
-    bot.start(bot_token=BOT_TOKEN)
-except Exception as er:
-    LOGS.info(er)
-
-####### GENERAL CMDS ########
-
-@bot.on(events.NewMessage(pattern="/start"))
-async def _(e):
-    await start(e)
-
-
-
-
-@bot.on(events.NewMessage(pattern="/setcode"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await coding(e)
-
-
-@bot.on(events.NewMessage(pattern="/getcode"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getcode(e)
-
-
-@bot.on(events.NewMessage(pattern="/showthumb"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getthumb(e)
-
-
-@bot.on(events.NewMessage(pattern="/logs"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await getlogs(e)
-
-
-@bot.on(events.NewMessage(pattern="/cmds"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await zylern(e)
-
-
-@bot.on(events.NewMessage(pattern="/ping"))
-async def _(e):
-    await up(e)
-
-
-@bot.on(events.NewMessage(pattern="/sysinfo"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await sysinfo(e)
-
-
-@bot.on(events.NewMessage(pattern="/leech"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await dl_link(e)
-
-
-@bot.on(events.NewMessage(pattern="/help"))
-async def _(e):
-    await ihelp(e)
-
-
-@bot.on(events.NewMessage(pattern="/renew"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await renew(e)
-
-
-@bot.on(events.NewMessage(pattern="/clear"))
-async def _(e):
-    if str(e.sender_id) not in OWNER and e.sender_id != DEV:
-        return e.reply("**Sorry You're not An Authorised User!**")
-    await clearqueue(e)
-
-
-@bot.on(events.NewMessage(pattern="/speed"))
-async def _(e):
-    await test(e)
-
-
-########## Direct ###########
-
-@bot.on(events.NewMessage(pattern="/eval"))
-async def _(e):
-    await eval(e)
-
-
-@bot.on(events.NewMessage(pattern="/bash"))
-async def _(e):
-    await bash(e)
-
-
-######## Callbacks #########
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats(.*)")))
-async def _(e):
-    await stats(e)
-
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile(b"skip(.*)")))
-async def _(e):
-    await skip(e)
-
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile("help")))
-async def _(e):
-    await help(e)
-
+# Other command definitions go here with proper indentation
 
 ########## AUTO ###########
 
@@ -278,11 +35,74 @@ async def _(event):
     await event.client.download_media(event.media, file="/bot/thumb.jpg")
     await event.reply("**Thumbnail Saved Successfully.**")
 
+async def something():
+    for i in itertools.count():
+        try:
+            if not WORKING and QUEUE:
+                user = int(OWNER.split()[0])
+                e = await bot.send_message(user, "**📥 Downloading Queue Files...**")
+                s = dt.now()
+                try:
+                    if isinstance(QUEUE[list(QUEUE.keys())[0]], str):
+                        dl = await fast_download(
+                            e, list(QUEUE.keys())[0], QUEUE[list(QUEUE.keys())[0]]
+                        )
+                    else:
+                        dl, file = QUEUE[list(QUEUE.keys())[0]]
+                        tt = time.time()
+                        dl = "downloads/" + dl
+                        with open(dl, "wb") as f:
+                            ok = await download_file(
+                                client=bot,
+                                location=file,
+                                out=f,
+                                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                                    progress(
+                                        d,
+                                        t,
+                                        e,
+                                        tt,
+                                        f"**📥 Downloading**\n__{dl.replace(f'downloads/', '')}__",
+                                    )
+                                ),
+                            )
+                except Exception as r:
+# This file is part of the CompressorQueue distribution.
+# Copyright (c) 2021 Danish_00
+# Script Improved by Zylern
+
+import itertools
+from . import *
+from .config import *
+from .worker import *
+from .devtools import *
+from .FastTelethon import *
+
+LOGS.info("Starting...")
+
+try:
+    bot.start(bot_token=BOT_TOKEN)
+except Exception as er:
+    LOGS.info(er)
+
+
+####### GENERAL CMDS ########
+
+@bot.on(events.NewMessage(pattern="/start"))
+async def _(e):
+    await start(e)
+
+# Other command definitions go here with proper indentation
+
+########## AUTO ###########
 
 @bot.on(events.NewMessage(incoming=True))
-async def _(e):
-    await encod(e)
-
+async def _(event):
+    if not event.photo:
+        return
+    os.system("rm thumb.jpg")
+    await event.client.download_media(event.media, file="/bot/thumb.jpg")
+    await event.reply("**Thumbnail Saved Successfully.**")
 
 async def something():
     for i in itertools.count():
@@ -388,10 +208,61 @@ async def something():
         except Exception as err:
             LOGS.info(err)
 
-
 ########### Start ############
 
 LOGS.info("Bot has started.")
 with bot:
     bot.loop.run_until_complete(something())
     bot.loop.run_forever()
+￼Enter                    LOGS.info(r)
+                    WORKING.clear()
+                    QUEUE.pop(list(QUEUE.keys())[0])
+                es = dt.now()
+                kk = dl.split("/")[-1]
+                aa = kk.split(".")[-1]
+                newFile = dl.replace(f"downloads/", "").replace(f"_", " ")
+                rr = "encode"
+                bb = kk.replace(f".{aa}", ".mkv")
+                out = f"{rr}/{bb}"
+                thum = "thumb.jpg"
+                dtime = ts(int((es - s).seconds) * 1000)
+                hehe = f"{out};{dl};{list(QUEUE.keys())[0]}"
+                wah = code(hehe)
+                nn = await e.edit(
+                    "**🗜 Compressing...**",
+                    buttons=[
+                        [Button.inline("STATS", data=f"stats{wah}")],
+                        [Button.inline("CANCEL", data=f"skip{wah}")],
+                    ],
+                )
+                cmd = f"""ffmpeg -i "{dl}" {ffmpegcode[0]} "{out}" -y"""
+                process = await asyncio.create_subprocess_shell(
+                    cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+                )
+                stdout, stderr = await process.communicate()
+          er = stderr.decode()
+                try:
+                    if er:
+                        await e.edit(str(er) + "\n\n**ERROR**")
+                        QUEUE.pop(list(QUEUE.keys())[0])
+                        os.remove(dl)
+                        os.remove(out)
+                        continue
+                except BaseException:
+                    pass
+                ees = dt.now()
+                ttt = time.time()
+                await nn.delete()
+                nnn = await e.client.send_message(e.chat_id, "**📤 Uploading...**")
+                with open(out, "rb") as f:
+                    ok = await upload_file(
+                        client=e.client,
+                        file=f,
+                        name=out,
+                        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                            progress(d, t, nnn, ttt, f"**📤 Uploading**\n__{out.replace(f'encode/', '')}__")
+                        ),
+                    )
+                await nnn.delete()
+                org = int(Path(dl).stat().st_size)
+                com = int(Path(out).stat().st_size)
